5a8c63113c17eece745cf983b46c5bd33ac2a277
Reimeister <128944245+Reimeister@users.noreply.github.com>
2023-05-03 16:13:26 -0500

    Add basic operations, button skeleton

---
+++ calculator.js
@@ -0,0 +1,16 @@
+//This is the JS file for the calculator project
+function add(firstNum, secondNum) {
+  return firstNum + secondNum;
+}
+
+function subtract(firstNum, secondNum) {
+  return firstNum - secondNum;
+}
+
+function multiply(firstNum, secondNum) {
+  return firstNum * secondNum;
+}
+
+function divide(firstNum, secondNum) {
+  return firstNum / secondNum;
+}
